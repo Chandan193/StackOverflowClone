c0f4128d4d37a3761e7aee28b26ae542a2c5201d
Chandan193 <ghoshchandan1907@gmail.com>
2024-02-01 23:05:08 +0530

    ..

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import "../../App.css";
-import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
-import RightSidebar from "./components/RightSidebar/RightSidebar";
-import HomeMainbar from "./components/HomeMainbar/HomeMainbar";
+import "../../App.css"
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
+import HomeMainbar from "../../components/HomeMainbar/HomeMainbar";
 
 const Home = ({ slideIn }) => {
   return (
